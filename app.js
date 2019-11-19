09d1d7ab5bbd53d8409f7156207495ce36127f8b
EC2 Default User <ec2-user@ip-172-31-42-25.us-east-2.compute.internal>
2019-11-19 18:31:44 +0000

    fixing execution

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const app = express();
+app.engine('html',require('ejs').renderFile);
+app.use(express.static("public"));
+
+//import faker and get a word
+//NOTE: I TRIED GETTING 7 DIFFERENT WORDS AND IT LEAD TO A TIMEOUT
+var faker = require('faker');
+var word1 = faker.hacker.adjective();
+
+//routes
+app.get ("/", function (req, res) {
+    res.render("index.ejs", {fakeword1:word1});
+})
+
+app.get("/companies", function(req, res){
+    res.render("companies.ejs", {fakeword1:word1});
+});
+
+app.get("/future", function(req, res){
+    res.render("future.ejs", {fakeword1:word1});
+});
+
+app.get("/how", function(req, res){
+    res.render("how.ejs", {fakeword1:word1});
+});
+
+
+//CLOUD 9 CONFIG
+app.listen("8080", "127.0.0.1", function(){
+    console.log("Express Server is Running...");
+});
+
+
+/*
+//HEROKU CONFIG
+//listner
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("Running Express Server...");
+});
+*/
