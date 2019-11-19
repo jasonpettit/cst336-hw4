8063993eb93be6937d51261ac7e6ffd57eded9de
EC2 Default User <ec2-user@ip-172-31-42-25.us-east-2.compute.internal>
2019-11-19 18:44:03 +0000

    fixing execution

--- app.js
+++ app.js
@@ -5,24 +5,24 @@ app.use(express.static("public"));
 
 //import faker and get a word
 //NOTE: I TRIED GETTING 7 DIFFERENT WORDS AND IT LEAD TO A TIMEOUT
-var faker = require('faker');
-var word1 = faker.hacker.adjective();
+//var faker = require('faker');
+//var word1 = faker.hacker.adjective();
 
 //routes
 app.get ("/", function (req, res) {
-    res.render("index.ejs", {fakeword1:word1});
+    res.render("index.ejs");
 })
 
 app.get("/companies", function(req, res){
-    res.render("companies.ejs", {fakeword1:word1});
+    res.render("companies.ejs");
 });
 
 app.get("/future", function(req, res){
-    res.render("future.ejs", {fakeword1:word1});
+    res.render("future.ejs");
 });
 
 app.get("/how", function(req, res){
-    res.render("how.ejs", {fakeword1:word1});
+    res.render("how.ejs");
 });
 
 /*
@@ -31,6 +31,7 @@ app.listen("8080", "127.0.0.1", function(){
     console.log("Express Server is Running...");
 });
 */
+
 
 //HEROKU CONFIG
 //listner
